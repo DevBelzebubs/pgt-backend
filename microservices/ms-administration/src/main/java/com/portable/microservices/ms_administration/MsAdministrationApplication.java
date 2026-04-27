102149e0bd34fa4b0430999642376b2c94693412
mythos <developerwebmythos@gmail.com>
2026-04-27 16:47:11 -0500

    config: estructura de proyecto y dockerizacion completada

---
+++ microservices/ms-administration/src/main/java/com/portable/microservices/ms_administration/MsAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.portable.microservices.ms_administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdministrationApplication.class, args);
+	}
+
+}
